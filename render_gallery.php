859ef60d73e423f706f9b26c7fb8420d7c538679
Oli Thompson <olithompson@rocketmail.com>
2020-02-08 15:05:41 +0000

    fix banner

--- render_gallery.php
+++ render_gallery.php
@@ -7,7 +7,57 @@
 </head>
 
 <div class="background">
-<?php include('partials/banner.php') ?>
+    <!-- Banner, Navigation bar and Sidenav Bar -->
+<div class="banner">
+    <div class="banner-content">
+        <img class="logo" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Website Icon.png" alt="Logo">
+        <h1>Oli&nbsp;Thompson</h1>
+        <div class="socialmediacontainer">
+            <a href="http://www.linkedin.com/in/oli-thompson" target="_blank">
+                <img class="socialmedialogo" alt="Linkedin" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/social_media_logos/Linkedin.svg"></a>
+            <a href="https://github.com/ot316" target="_blank">
+                <img class="socialmedialogo" alt="Github" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/social_media_logos/Github.svg"></a>
+            <a href="https://www.instagram.com/olithompson_designengineer/" target="_blank">
+                <img class="socialmedialogo" alt="Instagram" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/social_media_logos/Instagram.png"></a>
+        </div>
+    </div>
+</div>
+<img class="logomobile" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Website Icon.png" alt="Logo">
+<div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="index.php">
+        <p class="sidenavtext">Projects</p>
+    </a>
+    <a href="about_me.php">
+        <p class="sidenavtext">About&nbsp;Me</p>
+    </a>
+    <a href="render_gallery.php">
+        <p class="sidenavtext">Render&nbsp;Gallery</p>
+    </a>
+    <a href="digital_art.php">
+        <p class="sidenavtext">Digital&nbsp;Art</p>
+    </a>
+    <div class="socialmediacontainermobile">
+        <a href="https://www.instagram.com/olithompson_designengineer/" target="_blank">
+            <img class="socialmedialogomobile" alt="Instagram" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/social_media_logos/Instagram.png"></a>
+        <a href="https://github.com/ot316" target="_blank">
+            <img class="socialmedialogomobile" alt="Github" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/social_media_logos/Github.svg"></a>
+        <a href="http://www.linkedin.com/in/oli-thompson" target="_blank">
+            <img class="socialmedialogomobile" alt="Linkedin" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/social_media_logos/Linkedin.svg"></a>
+    </div>
+</div>
+<span class="sidenavbutton" onclick="openNav()">&#9776;</span>
+<div class="navbarwrapper">
+    <div class="navbar">
+        <ul>
+            <li><a href="index.php">Projects</a></li>
+            <li><a href="about_me.php">About Me</a></li>
+            <li><a class="active" href="render_gallery.php"> Render Gallery</a></li>
+            <li><a href="digital_art.php">Digital Art</a></li>
+        </ul>
+    </div>
+</div>
+<br>
     <div class="wrapper">
         <!-- Heading -->
         <p> All original renders, some are part of projects and others are independent.</p>
