92502b55d8d171607792bd1950bc7305aba0772b
Oli Thompson <olithompson@rocketmail.com>
2020-02-08 14:55:17 +0000

    make it look decent

--- mail.php
+++ mail.php
@@ -40,25 +40,27 @@ $mail->send();
     <?php include('partials/banner.php') ?>
     <div class="wrapper">
         <!-- Heading -->
-        <img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/aboutmepic.png" style=" float: left; padding-top: 20px; padding-right: 25px; padding-bottom: 15px; max-width: 20%;">
+        <img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/aboutmepic.png" style=" float: left; padding-top: 20px; padding-right: 25px; padding-bottom: 15px; max-width: 18%;">
         <p style="display: block; padding-top: 20px;"> I'm Oli, a 4th year Design Engineering Masters Student at Imperial College London, continuing my studies with a postgraduate masters in computing science before moving into industry.
         </p>
-        <p style="display: block"> I have a deep passion for learning about robotics, technology and engineering and am always working to broaden and deepen my knowledge through self-directed study and personal project work.
+        <p style="display: block"> I have a deep passion for learning about technology, robotics and engineering and am always working to broaden and deepen my knowledge through self-directed study and personal project work.
         </p>
         <p style="display: block; ">
-            I can carry engineering projects through each level of development, from ideation and user experience design through mechanical and electronic challenges down to the algorithms that make it work and a final prototype.
+            I enjoy carrying engineering projects through each level of development, from ideation and user experience design through mechanical and electronic challenges down to the software that make it work and a final prototype.
         </p>
-        <p style="display: block; padding-bottom: 10px;">
-            If you think my engineering and prototyping skills could be of use to your project or you have any questions about my work, please contact me at <a href=" mailto: olithompson@rocketmail.com ">
-                olithompson@rocketmail.com.</a>
-        </p>
-        <h3 style="padding-top: 0px; padding-bottom: 60px; display: block; font-size: 14px; ">Click <a href="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Oli Thompson CV.pdf" target="_blank">
-                here</a> to download my CV</h3>
-                <h3 style ="font-size: 20px;"> Thanks for your message </h3>
-                <br>
-                <br>
-                <br>
-                <img class="hero " src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/software.png ">
+ 
+        <p style="display: block; ">Please click<a href="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Oli Thompson CV.pdf" target="_blank">
+        <strong> here </strong></a> to download my CV.</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <h3>Software Competencies</h3>
+        <br>
+        <img class="hero " src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/software.png ">
+        <br>
+        <br>
+        <br>
     </div>
 </div>
 <?php include('partials/footer.php') ?>
