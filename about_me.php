c1611aa42484a525c6b5646338e6e3bc065be673
Oli Thompson <olithompson@rocketmail.com>
2020-02-08 14:47:57 +0000

    make it look decent

--- about_me.php
+++ about_me.php
@@ -10,23 +10,21 @@
     <?php include('partials/banner.php') ?>
     <div class="wrapper">
         <!-- Heading -->
-        <img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/aboutmepic.png" style=" float: left; padding-top: 20px; padding-right: 25px; padding-bottom: 15px; max-width: 20%;">
+        <img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/aboutmepic.png" style=" float: left; padding-top: 20px; padding-right: 25px; padding-bottom: 15px; max-width: 18%;">
         <p style="display: block; padding-top: 20px;"> I'm Oli, a 4th year Design Engineering Masters Student at Imperial College London, continuing my studies with a postgraduate masters in computing science before moving into industry.
         </p>
-        <p style="display: block"> I have a deep passion for learning about robotics, technology and engineering and am always working to broaden and deepen my knowledge through self-directed study and personal project work.
+        <p style="display: block"> I have a deep passion for learning about technology, robotics and engineering and am always working to broaden and deepen my knowledge through self-directed study and personal project work.
         </p>
         <p style="display: block; ">
             I enjoy carrying engineering projects through each level of development, from ideation and user experience design through mechanical and electronic challenges down to the software that make it work and a final prototype.
         </p>
-
-        <br>
-        <br>
-        <h3 style="padding-top: 0px; padding-bottom: 40px; display: block; font-size: 14px; ">Click <a href="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Oli Thompson CV.pdf" target="_blank">
-                here</a> to download my CV</h3>
-                <br>
+ 
+        <p style="display: block; ">Please click<a href="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Oli Thompson CV.pdf" target="_blank">
+        <strong> here </strong></a> to download my CV.</p>
+                <br><br>
+        <h3>Contact Form </h3>
         <br>
         <div style="border-style: solid; margin: auto; padding: 20px; max-width: 60%";>
-        <h3>Contact Form </h3>
             <form action="mail.php" method="POST">
                 <p style="margin: 7px;">Name</p> <input type="text" name="name">
                 <p style="margin: 7px;">Email</p> <input type="text" name="email">
