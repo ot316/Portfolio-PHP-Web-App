c161c71a3d9f718882940eee0e83c6101b971c5a
ot316 <olithompson@rocketmail.com>
2020-02-11 00:40:00 +0000

    resize picture

--- about_me.php
+++ about_me.php
@@ -108,7 +108,7 @@ function test_input($data)
     <br>
     <div class="wrapper">
         <!-- Heading -->
-        <img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/aboutmepic.png" style=" float: left; padding-top: 20px; padding-right: 25px; padding-bottom: 15px; max-width: 18%;">
+        <img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/aboutmepic.png" style=" float: left; padding-top: 20px; padding-right: 25px; padding-bottom: 15px; max-width: 22%;">
         <p style="display: block; padding-top: 20px;"> I'm a 4th year Design Engineering Masters Student at Imperial College London, continuing my studies with a postgraduate masters in computing science before moving into industry.
         </p>
         <p style="display: block"> I have a deep passion for learning about technology, robotics and engineering and am always working to broaden and deepen my knowledge through self-directed study and personal project work.
