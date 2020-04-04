567889f22b68d5a43c65101b73d1ea813adf7aa0
ot316 <olithompson@rocketmail.com>
2020-04-04 15:37:44 +0100

    remove.php file extensions

--- partials/head_homepage.php
+++ partials/head_homepage.php
@@ -1,10 +1,4 @@
 <!-- Header -->
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
-<link rel="stylesheet" href="./StylesHomePage.css">
-<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&display=swap" rel="stylesheet">
-<link rel="icon" href="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Website Icon.png">
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-159632500-1"></script>
@@ -15,3 +9,11 @@
 
   gtag('config', 'UA-159632500-1');
 </script>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="./StylesHomePage.css">
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&display=swap" rel="stylesheet">
+<link rel="icon" href="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Website Icon.png">
+
