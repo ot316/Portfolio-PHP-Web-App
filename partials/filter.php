a204c3dcaa65e1780292f0a83abf96d1bdb66cbe
ot316 <olithompson@rocketmail.com>
2020-04-04 15:19:39 +0100

    remove.php file extensions

--- partials/filter.php
+++ partials/filter.php
@@ -1,58 +1,58 @@
 <p> Welcome to my design, engineering and technology portfolio website. Please use the drop down filter below to sort through my projects by skillset.</p>
 <div class="dropdown">
     <button onclick="myFunction()" id="dropbtn" class="dropbtn"><img class="dropdowngraphic" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/dropdowngraphic.png" style="filter: invert(100%)">Filter</button>
-    <a href="index.php">
+    <a href="index">
         <button class="dropbtn"><img class="dropdowngraphic" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/closemenugraphic.png" style="filter: invert(100%)">Remove Filter</button>
     </a>
     <div id="myDropdown" class="dropdown-content">
 
         <div class="dropdownhover">
-            <a href="filter_electronics.php">
+            <a href="filter_electronics">
                 <h2>Electronics</h2>
             </a>
             <div class="dropdown-content-hover">
-                <a href="filter_pcb_design.php">PCB Design</a>
-                <a href="filter_signal_processing.php">Signal Processing</a>
-                <a href="filter_control_systems.php">Control Systems</a>
-                <a href="filter_mechatronics.php">Mechatronics</a>
+                <a href="filter_pcb_design">PCB Design</a>
+                <a href="filter_signal_processing">Signal Processing</a>
+                <a href="filter_control_systems">Control Systems</a>
+                <a href="filter_mechatronics">Mechatronics</a>
             </div>
         </div>
         <h3>|</h3>
         <div class="dropdownhover">
-            <a href="filter_software.php">
+            <a href="filter_software">
                 <h2>Software</h2>
             </a>
             <div class="dropdown-content-hover">
-                <a href="filter_computer_vision.php">Computer Vision</a>
-                <a href="filter_machine_learning.php">Machine Learning</a>
-                <a href="filter_robotics.php">Robotics</a>
-                <a href="filter_web_and_network.php">Web and Network</a>
+                <a href="filter_computer_vision">Computer Vision</a>
+                <a href="filter_machine_learning">Machine Learning</a>
+                <a href="filter_robotics">Robotics</a>
+                <a href="filter_web_and_network">Web and Network</a>
             </div>
         </div>
         <h3>|</h3>
         <div class="dropdownhover">
-            <a href="filter_mechanical.php">
+            <a href="filter_mechanical">
                 <h2>Mechanical</h2>
             </a>
             <div class="dropdown-content-hover">
-                <a href="filter_technical_drawing.php">Technical Drawings</a>
-                <a href="filter_analytical_engineering.php">Analytical Engineering</a>
-                <a href="filter_cad.php">CAD</a>
-                <a href="filter_cfd.php">CFD</a>
-                <a href="filter_fea.php">FEA</a>
+                <a href="filter_technical_drawing">Technical Drawings</a>
+                <a href="filter_analytical_engineering">Analytical Engineering</a>
+                <a href="filter_cad">CAD</a>
+                <a href="filter_cfd">CFD</a>
+                <a href="filter_fea">FEA</a>
             </div>
         </div>
         <h3>|</h3>
 
         <div class="dropdownhover">
-            <a href="filter_design.php">
+            <a href="filter_design">
                 <h2>Design</h2>
             </a>
             <div class="dropdown-content-hover">
-                <a href="filter_product_design.php">Product Design</a>
-                <a href="filter_systems_design.php">Systems Design</a>
-                <a href="filter_prototyping.php">Prototyping</a>
-                <a href="filter_ux.php">UX</a>
+                <a href="filter_product_design">Product Design</a>
+                <a href="filter_systems_design">Systems Design</a>
+                <a href="filter_prototyping">Prototyping</a>
+                <a href="filter_ux">UX</a>
             </div>
         </div>
     </div>
