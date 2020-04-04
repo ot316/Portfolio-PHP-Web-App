3f97b00cc288dfe66f853c474ef5f1611c6ec8ac
ot316 <olithompson@rocketmail.com>
2020-04-04 19:29:12 +0100

    add filter controls

---
+++ filter_control.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include('partials/head.php') ?>
+    <title>Control</title>
+</head>
+
+<div class="background">
+    <?php include('partials/banner.php') ?>
+    <div class="wrapper">
+        <!-- Heading -->
+        <?php include('partials/filter.php') ?>
+        <!-- Project Pictures -->
+        <div class="bigpicwrapper" id="bigpicwrapper">
+            <div class="picwrapper" id="picwrapper">
+                <?php include('project_partials/zero_gravity_garden.php') ?>
+                <?php include('project_partials/baxter_robot.php') ?>
+                <?php include('project_partials/gizmo.php') ?>
+            </div>
+            <div class="picwrapper" id="picwrapper">
+                <?php include('project_partials/autonomous_drone.php') ?>
+                <?php include('project_partials/empty.php') ?>
+                <?php include('project_partials/empty.php') ?>
+
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include('partials/footer.php') ?>
+<script src="../javascript/pic_wrapper.js"></script>
+<script src="../javascript/navbar.js"></script>
+
+
+</html>
--- project_partials/linear_regression.php
+++
@@ -1,8 +0,0 @@
-<div class="piccontainer" id="picwrapper">
-    <a href="linear_regression" class="links">
-        <img class="third" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/hero_images/LinearRegression.png" alt="Project">
-        <div class="middle">
-            <h4>Linear Regression</h4>
-        </div>
-    </a>
-</div>
