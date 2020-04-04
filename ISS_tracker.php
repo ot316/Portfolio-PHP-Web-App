30c91ba391cb07f8597b254162f4328e89dbf642
ot316 <olithompson@rocketmail.com>
2020-04-04 21:49:16 +0100

    fix footer

--- ISS_tracker.php
+++ ISS_tracker.php
@@ -8,7 +8,7 @@
     <script src="javascript/p5/p5.min.js"></script>
 </head>
 
-<div class="background" >
+<body class="background">
     <?php include('partials/banner.php') ?>
     <div class="wrapper">
         <div>
@@ -28,16 +28,17 @@
                 <li>I setup a Raspberry Pi to send me an email whenever the International Space Station is over London.</li>
             </ul>
             <div id="cnv" class="hero"></div>
-            <div id="spacing"><div>
-            <h3 style="padding-bottom: 20px;" id="text"></h3>
-        </div>
-        <h2>How it Works</h2>
-        <h3 style="padding-top: 10px;">Flow Diagram</h3>
-        <img class="hero" style="padding-top: 10px; padding-bottom: 20px; max-width: 250px;" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/ISS_tracker/flow.png">
+            <div id="spacing">
+                <div>
+                    <h3 style="padding-bottom: 20px;" id="text"></h3>
+                </div>
+                <h2>How it Works</h2>
+                <h3 style="padding-top: 10px;">Flow Diagram</h3>
+                <img class="hero" style="padding-top: 10px; padding-bottom: 20px; max-width: 250px;" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/ISS_tracker/flow.png">
 
-        <p>The Javascript code below is running on the client device. It polls the ISS API every 1.5 seconds, converting the returned latitude and longitude into a mercator projection. Further code uses the P5 library to display the graphics on the webpage. All code can be viewed on my Github.
-        </p>
-        <PRE class="code">complete: function (data) {
+                <p>The Javascript code below is running on the client device. It polls the ISS API every 1.5 seconds, converting the returned latitude and longitude into a mercator projection. Further code uses the P5 library to display the graphics on the webpage. All code can be viewed on my Github.
+                </p>
+                <PRE class="code">complete: function (data) {
 if (data.readyState === 4 && data.status === 200) {
     Lat = data.responseJSON.latitude;
     Long = data.responseJSON.longitude;
@@ -47,10 +48,10 @@ if (data.readyState === 4 && data.status === 200) {
     var latRad = (parseFloat(Lat) * Math.PI / 180);
     var mercN = (Math.log(Math.tan((Math.PI / 4) + (latRad / 2))));
     var y = (MapHeight / 2) - (MapWidth * mercN / (2 * Math.PI));</PRE>
-        <p>The Python code below sends an email if the latitude and longitude values are within a certain range. The "findISS" function works in a very similar way to the javascript implementation above. I run the code continuously on a Raspberry Pi. </p>
-        <h3 style="padding-bottom: 20px;"> Raspberry Pi Setup</h3>
-        <img class="hero" style="padding-top: 10px; padding-bottom: 20px; max-width: 500px;" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/ISS_tracker/pi.jpg">
-        <PRE class="code">server = smtplib.SMTP('smtp.gmail.com', 587)
+                <p>The Python code below sends an email if the latitude and longitude values are within a certain range. The "findISS" function works in a very similar way to the javascript implementation above. I run the code continuously on a Raspberry Pi. </p>
+                <h3 style="padding-bottom: 20px;"> Raspberry Pi Setup</h3>
+                <img class="hero" style="padding-top: 10px; padding-bottom: 20px; max-width: 500px;" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/ISS_tracker/pi.jpg">
+                <PRE class="code">server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 while True:
     data = findISS()
@@ -61,8 +62,10 @@ while True:
         ISS overhead"""
         server.sendmail(sender_email, receiver_email, message)
         sleep(20)</PRE>
+            </div>
+        </div>
     </div>
-</div>
+</body>
 <?php include('partials/footer.php') ?>
 <script src="javascript/navbar.js"></script>
 <script src="javascript/ISS_Tracker.js"></script>
