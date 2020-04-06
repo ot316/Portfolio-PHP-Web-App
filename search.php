3ea7765a26b3a78dd95a254ca03810b62a0673ec
ot316 <olithompson@rocketmail.com>
2020-04-06 21:37:31 +0100

    add search read of database

---
+++ search.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "localhost";
+$username = "www-data";
+$password = "";
+$dbname = "olithompson";
+?>
+
+<?php
+if (isset($_POST['submit'])) {
+    if (preg_match("/[A-Z  | a-z]+/", $_POST['name'])) {
+        $name = $_POST['name'];
+        $name = htmlspecialchars($name);
+        trim($name);
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $sql = "SELECT * FROM search WHERE '$name' IN ('tag1','tag2','tag3','tag4','tag5','tag6','tag7','tag8','tag9', 'tag10')";
+        $result = $conn->query($sql);
+        echo($result);
+    }
+} else {
+    echo ("<p>Please enter a search query</p>");
+}
