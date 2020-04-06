21bcfadc4ae66b3e0e6a10941666b6fed6fc6c3b
ot316 <olithompson@rocketmail.com>
2020-04-06 22:55:11 +0100

    add search

--- search.php
+++ search.php
@@ -23,8 +23,9 @@ if (isset($_POST['query'])) {
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-                echo "link: " . $row["link"];
+                echo $row["link"];
             }
+        }
     }
 
 } else {
