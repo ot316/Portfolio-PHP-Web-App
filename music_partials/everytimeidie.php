6debed5161cbaf5580c242372b7ed637d21b62c1
ot316 <olithompson@rocketmail.com>
2020-04-07 18:17:26 +0100

    add music section

---
+++ music_partials/everytimeidie.php
@@ -0,0 +1,10 @@
+<div class="piccontainer" id="picwrapper">
+    <a href="big_data" class="links">
+    <video class="herovideo" controls autoplay>
+            <source src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/music/everytimeidie.mp4" type="video/mp4">
+            Please change browser to view video.
+        </video>        <div class="middle2">
+            <h4>Everytime I Die</h4>
+        </div>
+    </a>
+</div>
