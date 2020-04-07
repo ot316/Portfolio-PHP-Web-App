71f86aebf73c9232f556ae67b1ed3d95fdcc81cc
ot316 <olithompson@rocketmail.com>
2020-04-07 20:30:10 +0100

    fix music partials

--- music_partials/asiam.php
+++ music_partials/asiam.php
@@ -1,10 +1,9 @@
 <div class="piccontainer" id="picwrapper">
-    <a href="big_data" class="links">
     <video class="herovideo" controls>
-            <source src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/music/asiam.mp4" type="video/mp4">
-            Please change browser to view video.
-        </video>        <div class="middle2">
-            <h4>As I am</h4>
-        </div>
-    </a>
+        <source src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/music/asiam.mp4" type="video/mp4">
+        Please change browser to view video.
+    </video>
+    <div class="middle2">
+        <h4>As I am</h4>
+    </div>
 </div>
