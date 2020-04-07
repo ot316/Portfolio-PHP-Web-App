29f53ec63514e45b34aa34956f9e58f3b605f69e
ot316 <olithompson@rocketmail.com>
2020-04-07 20:24:50 +0100

    add music section

--- music_partials/everytimeidie.php
+++ music_partials/everytimeidie.php
@@ -1,6 +1,6 @@
 <div class="piccontainer" id="picwrapper">
     <a href="big_data" class="links">
-    <video class="herovideo" controls autoplay>
+    <video class="herovideo" controls>
             <source src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/music/everytimeidie.mp4" type="video/mp4">
             Please change browser to view video.
         </video>        <div class="middle2">
