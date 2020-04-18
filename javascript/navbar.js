014a151ed32cd49bf24597a403f30b2baedc10b8
ot316 <olithompson@rocketmail.com>
2020-04-18 12:54:05 +0100

    improve animation

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -1,12 +1,10 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("mySidenavcls").style.position = "absolute";
     document.getElementById("mySidenavcls").style.opacity = "1";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0px";
-    document.getElementById("mySidenavcls").style.position = "relative";
     document.getElementById("mySidenavcls").style.opacity = "0";
 
 
