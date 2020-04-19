9fd74f7af912b26649fd645bf1d52e8b6e24d1d9
ot316 <olithompson@rocketmail.com>
2020-04-19 11:28:24 +0100

    improve animation

--- project_partials/smart_mirror.php
+++ project_partials/smart_mirror.php
@@ -1,5 +1,5 @@
-<div class="piccontainer" id="picwrapper">
-    <a href="smart_mirror" class="links"  style="border:3px solid grey;">
+<div class="piccontainer" id="picwrapper" style="border:3px solid grey;">
+    <a href="smart_mirror" class="links">
         <img class="third" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/hero_images/smart_mirror.png">
         <div class="middle">
             <h4>Mood Tracking Smart Mirror</h4>
