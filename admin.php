09e913315fd968c484f123e43dff3ff129e5a3f9
ot316 <olithompson@rocketmail.com>
2020-04-20 12:00:43 +0100

    remove break

--- admin.php
+++ admin.php
@@ -35,8 +35,10 @@ $dbname = "olithompson";
     </div>
     <img class="logomobile" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Website Icon.png" alt="Logo">
     <div id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="/" style="margin-top: 75px;">
+        <a href="" onclick="closeNav()">
+            <p style="font-size: 36px;" class="sidenavtext">&times;</p>
+        </a>
+        <a href="/">
             <p class="sidenavtext">Projects</p>
         </a>
         <a href="about_me">
