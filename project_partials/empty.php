f8b90c2be2facbaef88947f21ab6fca771b18ce8
ot316 <olithompson@rocketmail.com>
2020-04-21 12:06:26 +0100

    remove placeholder image

--- project_partials/empty.php
+++ project_partials/empty.php
@@ -1,3 +1,3 @@
 <div class="piccontainer" id="picwrapper">
-    <img class="thirdplaceholder" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/placeholder.png">
+    <div class="thirdplaceholder"></div>
 </div>
