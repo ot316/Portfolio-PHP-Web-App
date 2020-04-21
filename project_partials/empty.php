37eae0a4a1939feb1bdeba94e53c87e5a989323f
ot316 <olithompson@rocketmail.com>
2020-04-21 12:09:41 +0100

    remove placeholder image

--- project_partials/empty.php
+++ project_partials/empty.php
@@ -1,4 +1,5 @@
 <div class="piccontainer" id="picwrapper">
     <div class="thirdplaceholder">
-        <div style="width: 100px; height: 100px;"></div></div>
+        <div style="width: 150px; height: 100px;"></div>
+    </div>
 </div>
