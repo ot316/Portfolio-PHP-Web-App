af906209509324ce18ca5387573d88e0b9d8b92e
ot316 <olithompson@rocketmail.com>
2020-04-21 12:13:47 +0100

    fix border issuew mobile

--- partials/filter.php
+++ partials/filter.php
@@ -1,4 +1,3 @@
-<link rel="stylesheet" href="./StylesHomePage.css">
 <p class="mobilehidetext"> Welcome to my design, engineering and technology portfolio website. Please use the drop down filter below to sort through my projects by skillset.</p>
 <div class="dropdown">
     <button onclick="myFunction()" id="dropbtn" class="dropbtn"><img class="dropdowngraphic" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/dropdowngraphic.png" style="filter: invert(100%)">Filter</button>
