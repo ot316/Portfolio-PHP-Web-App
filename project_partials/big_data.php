8186ebfa620aeac47cd132a3220e461d1e7ade56
ot316 <olithompson@rocketmail.com>
2020-04-21 11:48:57 +0100

    fix border issuew mobile

--- project_partials/big_data.php
+++ project_partials/big_data.php
@@ -1,4 +1,4 @@
-<div class="piccontainerborder" id="picwrapper"">
+<div class="piccontainerborder" id="picwrapper">
     <a href="big_data" class="links">
         <img class="third" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/hero_images/big_data.png" alt="Project">
         <div class="middle">
