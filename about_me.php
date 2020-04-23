4c6c7e6ddf4ebd1369c586a8e253204f4c63cf53
ot316 <olithompson@rocketmail.com>
2020-04-23 22:27:40 +0100

    fix from buttons

--- about_me.php
+++ about_me.php
@@ -170,7 +170,7 @@ function test_input($data)
                 <span class="error">* <?php echo $emailErr; ?></span>
                 <p style="margin: 7px;">Message</p><textarea name="message" rows="6" cols="35"></textarea><br />
                 <br>
-                <input style="padding: 5px 15px; margin-right: 20px; background-color: lightgrey; color: white;" type="submit" value="Send"><input style="padding: 5px 15px; background-color: lightgrey; color: white;" type="reset" value="Clear">
+                <input style="padding: 8px 20px; margin-right: 15px; background-color: lightgrey; color: white;" type="submit" value="Send"><input style="padding: 8px 20px; background-color: lightgrey; color: white;" type="reset" value="Clear">
             </form>
         </div>
         <br>
