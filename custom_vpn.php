65c8e9f15eb82c53d6639a3d190b47a76505a060
ot316 <olithompson@rocketmail.com>
2020-04-24 12:27:09 +0100

    change spacing

--- custom_vpn.php
+++ custom_vpn.php
@@ -16,6 +16,7 @@
                 <h2>Network</h2>
                 <h2>Linux</h2>
             </div>
+            <br>
             <!-- First Bullet Points -->
             <ul class="headingbullets">
                 <li>I rented a virtual machine in the cloud and used Open VPN to configure my own VPN server.</li>
@@ -23,6 +24,9 @@
             </ul>
             <img class="hero" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/custom_vpn/custom_vpn.png" alt="hero image">
             <h3 style="padding-top: 20px;">PiHole GUI</h3>
+            <br>
+            <br>
+            <h2>How it Works</h2>
             <p>The VPN runs on a linux VPS and is cheaper and arguably more secure than the market equivalent. Connection speed is increased as around 15% of DNS queries are blocked and I can add my own DNS blacklists from any connected device. </p>
         </div>
     </div>
