d20650015428b16a0b02039f9c78523c0baa6a5a
ot316 <olithompson@rocketmail.com>
2020-05-17 16:29:35 +0100

    add logo to other pages

--- admin.php
+++ admin.php
@@ -21,8 +21,10 @@ $dbname = "olithompson";
     <!-- Banner, Navigation bar and Sidenav Bar -->
     <div class="banner">
         <div class="banner-content">
-            <a href="/"><img class="logo" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Website Icon.png" alt="Logo"></a>
-            <h1>Oli&nbsp;Thompson</h1>
+            <?php include('logo.php') ?>
+            <a href="/">
+                <h1>Oli&nbsp;Thompson</h1>
+            </a>
             <div class="socialmediacontainer">
                 <a href="http://www.linkedin.com/in/oli-thompson" target="_blank">
                     <img class="socialmedialogo" alt="Linkedin" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/social_media_logos/Linkedin.svg"></a>
