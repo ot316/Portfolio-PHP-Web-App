8d383ee978811c36f8ec9ac8b6040ae4b79975d6
ot316 <olithompson@rocketmail.com>
2020-05-24 01:29:06 +0100

    reorganise css

--- partials/head.php
+++ partials/head.php
@@ -14,6 +14,6 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <link rel="stylesheet" href="./css/styles.css">
-<link rel="stylesheet" href="./css/styleshomepage.css">
+<link rel="stylesheet" href="./css/StylesHomePage.css">
 <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700&display=swap" rel="stylesheet">
 <link rel="icon" href="https://olithompson.s3.eu-west-2.amazonaws.com/Media/Misc/Website Icon.png">
