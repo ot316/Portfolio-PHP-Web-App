52691a482bcc24bdd73647584697e2e96a38fbfd
Oli Thompson <olithompson@rocketmail.com>
2020-10-31 12:05:31 +0000

    retrieve homepage, styles and featured partials

---
+++ featured_project_partials/magic_of_things.php
@@ -0,0 +1,6 @@
+<div class="featured_project_wrapper_right">
+    <img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/homepage/stump.png" alt="Project">
+    <h1>Magic Of Things</h1>
+    <p> Eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?</p>
+    <a href="magic_of_things"><h3>Read more ></h3></a>
+</div>
--- index.php
+++ index.php
@@ -12,8 +12,13 @@
     <?php include('partials/loader.php') ?>
     <?php include('partials/banner.php') ?>
     <div class="wrapper">
-        <p style="margin-bottom: 20px; font-size: 24px">Welcome to my website!</p>
+        <p style="margin-bottom: 20px; font-size: 24px">Project Highlights</p>
         <p style="margin-top: 0px; padding-bottom: 5px;" class="mobilehidetext">I am a Design Engineer and Computer Scientist who is particularly interested in bringing value to users through digital and physical products. Below are some project highlights from my time in industry and at University.</p>
+        <div class="big_featured_wrapper" id="bigpicwrapper">
+            <?php include('featured_project_partials/voxel.php') ?>
+            <?php include('featured_project_partials/magic_of_things.php') ?>
+            <?php include('featured_project_partials/voxel.php') ?>
+        </div>
     </div>
     <?php include('partials/footer.php') ?>
 </body>
