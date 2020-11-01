2e172ff702fd083a7835c1704afe9fd7b433ff4c
ot316 <olithompson@rocketmail.com>
2020-11-01 19:14:52 +0000

    add ltd

--- featured_project_partials/voxel.php
+++ featured_project_partials/voxel.php
@@ -1,6 +1,6 @@
 <div class="featured_project_wrapper_left">
 <a href="https://www.voxel.cc"><img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/homepage/voxel.jpg" alt="Project"></a>
     <a href="https://www.voxel.cc"><h2>Voxel</h2></a>
-    <p> Voxel is an IOT startup built on the principles of data privacy and hackability. Our modular line of products contain sensors, inputs and displays; powered by our open source software. <br> <br> My work involved developing the Voxel website and using it to validate interest with google analytics and a "fake door" purchase link. I communicated between users, contributers and potential investors and was also responsible for the hardware and software prototype development.</p>
+    <p> I co-founded Voxel, an IOT startup built on the principles of data privacy and hackability. Our modular line of products contain sensors, inputs and displays; powered by our open source software. <br> <br> My work involved developing the Voxel website and using it to validate interest with google analytics and a "fake door" purchase link. I communicated between users, contributers and potential investors and was also responsible for the hardware and software prototype development.</p>
     <a href="https://www.voxel.cc"><h3>Visit Website ></h3></a>
 </div>
