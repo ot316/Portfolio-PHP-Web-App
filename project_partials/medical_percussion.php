037cf6d2565f63a0326b601e20e4513d251ba476
ot316 <olithompson@rocketmail.com>
2020-11-01 15:07:49 +0000

    fix percussion link

--- project_partials/medical_percussion.php
+++ project_partials/medical_percussion.php
@@ -1,5 +1,5 @@
 <span class="piccontainer" id="picwrapper">
-    <a href="autonomous_drone" class="links">
+    <a href="medical_percussion" class="links">
         <img class="third" src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/hero_images/medical_percussion.png" alt="Project">
         <div class="middle">
             <h4>Medical Percussion</h4>
