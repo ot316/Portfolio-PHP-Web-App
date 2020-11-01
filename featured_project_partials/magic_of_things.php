8f9b69fe4a2a7a9030ae88d7d24a5aedaad423d0
ot316 <olithompson@rocketmail.com>
2020-11-01 15:12:48 +0000

    make links cover image homepage

--- featured_project_partials/magic_of_things.php
+++ featured_project_partials/magic_of_things.php
@@ -1,6 +1,6 @@
 <div class="featured_project_wrapper_right">
-    <img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/homepage/stump.png" alt="Project">
-    <h2>Magic Of Things</h2>
+<a href="https://thecauldron.io/company/themagicofthings"><img src="https://olithompson.s3.eu-west-2.amazonaws.com/Media/homepage/stump.png" alt="Project"></a>
+<a href="https://thecauldron.io/company/themagicofthings"><h2>Magic Of Things</h2></a>
     <p> I spent 6 months working for the Magic of Things, a company that uses IOT tech to create magical experiences for magic themed cocktail bars around the world.<br><br> I designed several products including this interactive fairy house and roled them out to one of our venues to gather feedback from users and venue staff. After implementing improvements to address my findings the products were shipped out around the world. </p>
     <a href="https://thecauldron.io/company/themagicofthings"><h3>Visit Website ></h3></a>
 </div>
